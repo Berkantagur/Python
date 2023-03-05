3c38b644dc38b9df43041ad53feb2ebdea41cbf0
Berkantagur <berkantagur6@gmail.com>
2023-03-05 22:38:29 +0300

    .py

---
+++ enumerate1.py
@@ -0,0 +1,16 @@
+# Enumerate: The enumerate function in Python converts a data collection object into an enumerate object.
+people = ["Ayşenur", "Ahmet Furkan", "Beyza E.", "Kaan", "Petek", "Berkant", "Beyza Biryol"]
+
+def divide_people(people):
+    groups = [[], []]
+    for index, person in enumerate(people):
+        if index % 2 == 0:
+            groups[0].append(person)
+        else:
+            groups[1].append(person)
+    print(groups)
+    return groups
+
+bb = divide_people(people)
+print (bb[0])
+print (bb[1])
