3171b7969b0bf1b16df2dfe68ddcaa4f13adc416
Berkantagur <berkantagur6@gmail.com>
2023-03-05 20:47:12 +0300

    first commit

---
+++ exchangeCharacter.py
@@ -0,0 +1,18 @@
+
+def exchange (string):
+    new_string = ""
+
+#Navigating the indexes of the entered string
+    for string_index in range(len(string)):
+
+#If the index is even, capitalize it.
+        if string_index % 2 == 0:
+            new_string += string[string_index].upper()
+        
+#If the index is odd, lowercase it.      
+        else:
+            new_string += string[string_index].lower()
+    
+    print(new_string)
+
+exchange ("hi my name is berkant")
