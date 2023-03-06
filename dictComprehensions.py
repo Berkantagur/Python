6af5814570f045c70d7a88009a4705c8e92a7850
Berkantagur <berkantagur6@gmail.com>
2023-03-06 22:56:47 +0300

    .py

--- dictComprehensions.py
+++ dictComprehensions.py
@@ -1,3 +1,4 @@
+#Exp1
 dictionary = {'a': 4,
               'b': 3,
               'c': 6,
@@ -16,3 +17,18 @@ print(y)
 
 z = {k.upper(): v*3 for (k,v) in dictionary.items()}
 print(z)
+
+#Exp2: 
+#Purpose: It is desired to be added to a dictionary by taking the square of even numbers.
+
+numbers = range(21)
+new_dict = {}
+
+for n in numbers:
+    
+    if n % 2 == 0:
+        new_dict[n] = n ** 2
+
+print(new_dict)
+#OR 
+print({n: n**2 for n in numbers if n % 2 == 0})
