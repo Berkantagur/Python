37a6eb88405f257b7ae2ddd4e783a9f91ea14c37
Berkantagur <berkantagur6@gmail.com>
2023-03-08 18:49:37 +0300

    .py

--- reshaping.py
+++ reshaping.py
@@ -4,6 +4,11 @@
 
 import numpy as np
 
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+newarr = arr.reshape(2, 3, 2)
+print(arr)
+print(newarr)
+
 print(np.random.randint(1, 10, size=9))
 print(np.random.randint(1, 10, size=9).reshape(3,3))
 
